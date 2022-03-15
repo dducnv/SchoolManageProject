5f3f995e67ee29885c459f7ae5e633be7a17080b
Nguyen Van Duc <ducbe2k2@gmail.com>
2022-03-15 21:10:25 +0700

    commit layout

---
+++ SchoolManageASP/Data/MyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace bank_service.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext():base("name=SchoolManageDB")
+        {
+
+        }
+    }
+}
